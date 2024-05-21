c85cd5b844062ddbaf4a2995582afd2e6e19232e
HAMMOUDAmustaphaahmed <hammouda.ahmedmustapha@gmail.com>
2024-05-21 15:33:20 +0100

    version 1.0

---
+++ public/js/command.js
@@ -0,0 +1,115 @@
+var portfolio = "https://hammoudamustaphaahmed.github.io/MyResume.github.io/"
+var linkedin = "https://www.linkedin.com/in/hammouda-ahmed-a55270195/"
+var github = "https://github.com/HAMMOUDAmustaphaahmed"
+var blockchain = "https://hammoudamustaphaahmed.pythonanywhere.com/"
+var kryptify ="https://kryptify-web3-0-git-master-hammoudaahmedmustaphas-projects.vercel.app"
+var api="https://github.com/HAMMOUDAmustaphaahmed/RESTFUL-API"
+var email ="mailto:hammouda.ahmedmustapha@gmail.com"
+
+help = [
+    '+========================================================================================+',
+    '| Commands   | Descriptions                                                              |',
+    '+========================================================================================+',
+    '| whois      | Who is Brandon Ban?                                                       |',
+    '| hammouda      | Discover more about me                                                    |',
+    '| projects   | View Coding Projects                                                      |',
+    '| social     | Display Social Networks                                                   |',
+    '| history    | View command history                                                      |',
+    '| help       | Ah, yes. You\'re a command whisperer! You already know what this one does. |',
+    '| email      | Please don\'t spam my email                                                |',
+    '| clear      | Clear terminal                                                        |',
+    '| LinkedIn   | Checkout my LinkedIn profile for the latest updates!                      |',
+    '| GitHub     | Discover more projects on my GitHub profile                               |',
+                           
+    '+========================================================================================+',
+]
+
+whois = [
+    "<br>",
+    "Welcome to my Terminal Website, I'm <span class=\"command\">Hammouda Ahmed Mustapha</span>!😎",
+    "<br>",
+    "<br>",
+    "- As a <span class=\"command\">highly motivated</span> and <span class=\"command\">disciplined</span> computer engineer, I am deeply committed to pursuing a career in full stack development.",
+    "<br>",
+    "- Through a combination of <span class=\"command\">self-study</span> and collaboration with my peers,",
+    "  I am continuously working to hone my skills and achieve my goal of becoming a full-fledged <span class=\"command\">full stack developer</span>.",
+    "<br>",
+    "- I am also keen to <span class=\"command\">challenge myself</span> by venturing beyond my comfort zone and embracing new opportunities and challenges.",
+    "<br>",
+    "- One of the things I enjoy about programming is the opportunity to develop something that will <span class=\"command\">make our lives easier and more efficient</span>.",
+    "<br>",
+    "- Apart from that, it allows me to collaborate with people from various disciplines who share my interests.",
+    "<br>",
+    "- It's fantastic to collaborate with other professionals who are striving toward the same goal.",
+    "<br>",
+]
+
+hammouda = [
+    "<br>",
+    'For a comprehensive overview of my qualifications and expertise, I kindly direct you to my <a href="' + portfolio + '" target="_blank">portfolio website.' + "</a>",
+    "<br>"
+]
+
+projects = [
+    "<br>",
+    "<span class=\"command\">Code Projects</span>",
+    '<a href="' + kryptify + '" target="_blank">Kryptify' + "</a>",
+    '<p class="terminal">Terminal Website (The one you\'re using right now!)</p>',
+   '<a href="' + blockchain + '" target="_blank">Blockchain Web3 Application' + "</a>",
+    
+    "<br>",
+]
+
+social = [
+    "<br>",
+    '<span class=\"command\">LinkedIn</span>       <a href="' + linkedin + '" target="_blank">LinkedIn/bbkx226' + "</a>",
+    '<span class=\"command\">GitHub</span>         <a href="' + github + '" target="_blank">GitHub/bbkx226' + "</a>",
+    "<br>"
+]
+
+banner = [
+  '▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄  ▄▄▄▄▄▄▄ ',
+  '█  █ █  █       █  █▄█  █  █▄█  █       █  █ █  █      ██       █',
+  '█  █▄█  █   ▄   █       █       █   ▄   █  █ █  █  ▄    █   ▄   █',
+  '█       █  █▄█  █       █       █  █ █  █  █▄█  █ █ █   █  █▄█  █',
+  '█   ▄   █       █       █       █  █▄█  █       █ █▄█   █       █',
+  '█  █ █  █   ▄   █ ██▄██ █ ██▄██ █       █       █       █   ▄   █',
+  '█▄▄█ █▄▄█▄▄█ █▄▄█▄█   █▄█▄█   █▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄██▄▄█ █▄▄█',
+  '                                                                                     ',
+  '                                                                                     ',
+  '                                                                                     ',
+  '.-----------------------------------------------------------------------------.                                      ue$$$$$$$$$e.',
+  '||Es| |F1 |F2 |F3 |F4 |F5 | |F6 |F7 |F8 |F9 |F10|                  BRANDON_   |                                   o$$$P""     "#$$$b',
+  '||__| |___|___|___|___|___| |___|___|___|___|___|                             |                                 d$$$"            ?$$N',
+  '| _____________________________________________     ________    ___________   |                                x$$$"               $$$',
+  '||~  |! |" |§ |$ |% |& |/ |( |) |= |? |` || |<-|   |Del|Help|  |{ |} |/ |* |  |                               d$$$     .ueeeeeeeeee$$$beeeeeu',
+  '||`__|1_|2_|3_|4_|5_|6_|7_|8_|9_|0_|ß_|\´_|\\_|__|   |___|____|  |[ |]_|__|__|  |                            @$$$     d$            $>      $$',
+  '||<-  |Q |W |E |R |T |Z |U |I |O |P |Ü |* |   ||               |7 |8 |9 |- |  |                             $$$$      $$            $>      4$',
+  '||->__|__|__|__|__|__|__|__|__|__|__|__|+_|_  ||               |__|__|__|__|  |          $$$.             .$$$P       $$            $>      4$',
+  '||Ctr|oC|A |S |D |F |G |H |J |K |L |Ö |Ä |^ |< |               |4 |5 |6 |+ |  |          ?$$$L           z$$$"        $$eeeeeeeeeeee$beeeeeed$',
+  '||___|_L|__|__|__|__|__|__|__|__|__|__|__|#_|__|       __      |__|__|__|__|  |            *$$$e..   .z$$$$"          $$                    4$',
+  '||^    |> |Y |X |C |V |B |N |M |; |: |_ |^     |      |A |     |1 |2 |3 |E |  |              ^#R$$$$$$$*"             $$                    4$',
+  '||_____|<_|__|__|__|__|__|__|__|,_|._|-_|______|    __||_|__   |__|__|__|n |  |                                       $$                    4$',
+  '|   |Alt|A  |                       |A  |Alt|      |<-|| |->|  |0    |. |t |  |                                       $$                    4$',
+  '|   |___|___|_______________________|___|___|      |__|V_|__|  |_____|__|e_|  |                                       $$                    4$',
+  '|                                                                             |                                       $$                    4$',
+  '.-----------------------------------------------------------------------------.                                       9$                    d$',
+  '                                                                                                                       $k                   $F',
+  '                                                                                                                        $N                z$" ',
+  '                                                                                                                         \'*$o.         .e$"  ',
+  '                                                                                                                              "#*$$$$P*""     ',
+]
+
+suggestion = [
+  "<p class>",
+  "<span class=\"color2\">Welcome to my terminal website! This website was inspired by <a target=\"_blank\" href=\>ForrestKnight~</a><br>For a list of available commands, type <span class=\"command\">help</span> .",
+  "</span>",
+  "</p>",
+]
+
+secret = [
+  "<br>",
+  '<span class=\"command\">sudo</span>       Only admin can type this command',
+  '<span class=\"command\">rm -rf</span>     This will remove all files and directories of this website',
+  "<br>"
+]
---
+++ public/js/main.js
@@ -0,0 +1,208 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+var password = "password"
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  loopLines(suggestion, "", 2500);
+  textarea.focus(); 
+  // It's used to set focus on a textarea element in HTML. 
+  // When an element has focus, it is ready to receive input from the user
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+// This code sets up an event listener for the keyup event on the window object
+// When the user releases a key on the keyboard, the enterKey function will be called
+
+console.log(
+  // %c is a placeholder for a style string
+  "%cHa Ha, proud of you, my \"inspect\" warrior friend!",
+  "color: #04ff00; font-weight: bold; font-size: 18px;"
+);
+
+console.log("%cVoila! The magical phrase you've been questing for! Password: '" + password + "' - I wonder what it does?🤔", "color: grey");
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) { // Audio mute key
+    document.location.reload(true); // the page will be reloaded with a hard reload
+  }
+  if (pw) {
+    let et = "*"; // often used to mask passwords by replacing each character with an asterisk
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) { // 13: Enter key
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("guest@MyLocalhost:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) { // 38: Arrow Up Key
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) { // 40: Arrow Down Key
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "bbkx":
+      loopLines(bbkx, "color2 margin", 80);
+      break;
+    case "sudo":
+      addLine("Oh no, you're not admin...", "color2", 80);
+      setTimeout(function() {
+        window.open();
+      }, 1000); 
+      break;
+    case "rm -rf":
+      addLine("HaHa, you really think you're going to make it, don't you?!", "color2", 80)
+      setTimeout(function() {
+        window.open('');
+      }, 1000); 
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "bmttuxd":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "password":
+      addLine("<span class=\"inherit\"> Ha ha ha! Nice try, my friend, but I'm not easily fooled! </span>", "error", 100);
+      addLine("<span class=\"inherit\"> You'll need to put on your best trickster hat if you want to pull one over on me! 😜 </span>", "error", 100);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine('Opening mailto:<a href="mailto:@gmail.com">@gmail.com</a>...', "color2", 80);
+      newTab(email);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    // socials
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 80);
+      newTab(linkedin);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    case "facebook":
+      addLine("Opening Facebook...", "color2", 0);
+      newTab(facebook);
+      break;
+    case "gcloud":
+      addLine("Opening Google Cloud Skills Boost...", "color2", 0);
+      newTab(gcloud);
+      break;
+    case "credly":
+      addLine("Opening Credly...", "color2", 0);
+      newTab(credly);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      // If the current character and the next character are both spaces, 
+      // &nbsp;&nbsp; is added to the t string to replace the two spaces.
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+    // used to insert the new next element before the before element
+    before.parentNode.insertBefore(next, before);
+    // The window.scrollTo() method is called with two arguments: 0 as the first argument to scroll to the top of the page, 
+    // and document.body.offsetHeight as the second argument to ensure that the new element is visible
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
